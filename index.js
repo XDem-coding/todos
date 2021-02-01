fb59c88ab46c5f8671360ab2c1e7e4c112bee546
XDem-coding <65942858+XDem-coding@users.noreply.github.com>
2021-02-01 18:11:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,79 @@
-priorityTextUpdate = () => {
-    let priorities = document.getElementsByClassName("priority")
-    for (let i = 0; i < priorities.length; i++) {
-        if (priorities[i].checked) {
-            document.getElementById('priority').innerText = priorities[i].parentElement.getElementsByTagName("label")[0].innerText;
-            if(i==0){
-                document.getElementById('priority').style.background = "#ffcccc";
-                document.getElementById('priority').style.color = "red";
-            }
-            else if(i==1){
-                document.getElementById('priority').style.background = "#ccccff";
-                document.getElementById('priority').style.color = "blue";
-            }
-            else if(i==2){
-                document.getElementById('priority').style.background = "#e6e6e6";
-                document.getElementById('priority').style.color = "black";
-            }
-        }
-    }
+/* Perfect */
+todayDate = () => {
+    var today = new Date();
+
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10){
+            dd='0'+dd
+        } 
+        if(mm<10){
+            mm='0'+mm
+        } 
+
+    today = yyyy+'-'+mm+'-'+dd;
+
+    return today;
 }
 
+/* Perfect */
 addNote = () => {
-    let title = document.getElementById('noteTitle').value.toUpperCase();
+    let title = document.getElementById('noteTitle').value;
     let description = document.getElementById('noteDescp').value;
-    if (description.length == 0) { description = '-' };
-    let priority = document.getElementById('priority').innerText;
-    const status = "pending";
+    let date = document.getElementById('noteDate').value;
+    if (date.length == 0) {date = todayDate()}
+    let status = "pending";
+
 
     if (title.length == 0) {
         document.getElementById('emptyField').style.display = 'block';
     }
+
     else {
         let data;
-        let success = document.getElementById('success')
+
         if (localStorage.getItem('notes') == null) {
-            data = [];
-            data.push({ "title": title, "description": description, "priority": priority, "status": status});
-            data = JSON.stringify(data);
-            localStorage.setItem('notes', data);
-            success.innerText = `Note added successfully: ${title}..!!`;
-            success.style.display = 'block';
-            document.getElementById('noteTitle').value = '';
-            document.getElementById('noteDescp').value = '';
-            document.getElementsByClassName("priority")[1].checked = true;
-            priorityTextUpdate();
+            data = []
         }
         else {
-            let already = false;
-            data = JSON.parse(localStorage.getItem('notes'));
-            data.forEach(item => {
-                if (item["title"] == title) {
-                    already = true;
-                }
-            })
-            if (already != true) {
-                data.push({ "title": title, "description": description, "priority": priority, "status": status});
-                data = JSON.stringify(data);
-                localStorage.setItem('notes', data);
-                success.innerText = `Note added successfully: ${title}..!!`;
-                success.style.display = 'block';
-                document.getElementById('noteTitle').value = '';
-                document.getElementById('noteDescp').value = '';
-                document.getElementsByClassName("priority")[1].checked = true;
-                priorityTextUpdate();
-            }
-            else {
-                document.getElementById('alreadyField').style.display = 'block';
-            }
-
+            data = JSON.parse(localStorage.getItem('notes'))
         }
-    }
 
-    showNotes();
+        data.push({ "title": title, "description": description, "date": date, "status": status })
+
+
+        data = JSON.stringify(data);
+        localStorage.setItem('notes', data);
+        document.getElementById('success').style.display = 'block';
+
+        document.getElementById('noteTitle').value = '';
+        document.getElementById('noteDescp').value = '';
+        document.getElementById('noteDate').value = '';
+
+        showNotes();
+    }
 }
 
+/* Perfect */
 showNotes = () => {
     let notes = localStorage.getItem('notes');
+
     let html = "";
     if (notes != null) {
+        /* 2 is the length of the string value in key notes*/
         if (notes.length != 2) {
             data = JSON.parse(notes);
+
             data.forEach(item => {
-                if (item["priority"].toLowerCase() == 'maximum' && item["status"].toLowerCase()== 'pending') {
+                if (item['status'] == 'pending' && (new Date(todayDate()) > new Date(item['date'])) ) {
                     html += `
                     <div class="card my-2 mx-auto border-danger text-danger" style="width: 18rem;background:#ffcccc;">
                         <div class="card-body text-center" style="position: relative;">
                             <h5 class="card-title">${item["title"]}</h5>
                             <hr>
+                            <h6 class="card-title">${item["date"]}</h6>
+                            <hr>
                             <p class="card-text">${item["description"]}</p>
                             <div class="btn-group" role="group" style="position: absolute;top: 10px;right: 0px;">
                                 <div class="dropdown">
@@ -104,14 +92,16 @@ showNotes = () => {
                     `
                 }
             })
-
+            
             data.forEach(item => {
-                if (item["priority"].toLowerCase() == 'normal' && item["status"].toLowerCase()== 'pending') {
+                if (item['status'] == 'pending' && (new Date(todayDate()) <= new Date(item['date'])) ) {
                     html += `
                     <div class="card my-2 mx-auto border-primary text-primary" style="width: 18rem;background:#ccccff;">
                         <div class="card-body text-center" style="position: relative;">
                             <h5 class="card-title">${item["title"]}</h5>
                             <hr>
+                            <h6 class="card-title">${item["date"]}</h6>
+                            <hr>
                             <p class="card-text">${item["description"]}</p>
                             <div class="btn-group" role="group" style="position: absolute;top: 10px;right: 0px;">
                                 <div class="dropdown">
@@ -132,38 +122,14 @@ showNotes = () => {
             })
 
             data.forEach(item => {
-                if (item["priority"].toLowerCase() == 'minimum' && item["status"].toLowerCase()== 'pending') {
-                    html += `
-                    <div class="card my-2 mx-auto border-dark text-dark" style="width: 18rem;background:#e6e6e6;">
-                        <div class="card-body text-center" style="position: relative;">
-                            <h5 class="card-title">${item["title"]}</h5>
-                            <hr>
-                            <p class="card-text">${item["description"]}</p>
-                            <div class="btn-group" role="group" style="position: absolute;top: 10px;right: 0px;">
-                                <div class="dropdown">
-                                    <button type="button" class="btn btn-sm btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <span class="visually-hidden">Toggle Dropdown</span>
-                                    </button>
-                                    <div class="dropdown-menu text-center" aria-labelledby="dropdownMenuButton">
-                                        <button type="button" class="deleteNote btn btn-outline-primary my-2" onClick="deleteNotes(${data.indexOf(item)})">Delete</button>
-                                        <button type="button" class="editNote btn  btn-outline-primary my-2" onClick="editButton(${data.indexOf(item)})">Edit</button>
-                                        <button type="button" class="btn  btn-outline-success my-2" onClick="toDone(${data.indexOf(item)})">Set as done</button>
-                                    </div>
-                                </div>   
-                            </div>
-                        </div>
-                    </div>
-                    `
-                }
-            });
-
-            data.forEach(item => {
-                if (item["status"].toLowerCase() == "done"){
+                if (item['status'] == 'done') {
                     html += `
                     <div class="card my-2 mx-auto border-success text-success" style="width: 18rem;background:#ccffcc;">
                         <div class="card-body text-center" style="position: relative;">
                             <h5 class="card-title">${item["title"]}</h5>
                             <hr>
+                            <h6 class="card-title">${item["date"]}</h6>
+                            <hr>
                             <p class="card-text">${item["description"]}</p>
                             <div class="btn-group" role="group" style="position: absolute;top: 10px;right: 0px;">
                                 <div class="dropdown">
@@ -173,7 +139,7 @@ showNotes = () => {
                                     <div class="dropdown-menu text-center" aria-labelledby="dropdownMenuButton">
                                         <button type="button" class="deleteNote btn btn-outline-primary my-2" onClick="deleteNotes(${data.indexOf(item)})">Delete</button>
                                         <button type="button" class="editNote btn  btn-outline-primary my-2" onClick="editButton(${data.indexOf(item)})">Edit</button>
-                                        <button type="button" class="btn  btn-outline-danger my-2" onClick="toPending(${data.indexOf(item)})">Set as pending</button>
+                                        <button type="button" class="btn btn-outline-danger my-2" onClick="toPending(${data.indexOf(item)})">Set as pending</button>
                                     </div>
                                 </div>   
                             </div>
@@ -181,9 +147,9 @@ showNotes = () => {
                     </div>
                     `
                 }
-            });
-
+            })
         }
+
         else {
             html = '<h5 class="card-title text-center">No saved notes yet..!!</h5>'
         }
@@ -195,6 +161,7 @@ showNotes = () => {
     document.getElementById('savedNotes').innerHTML = html;
 }
 
+/* Perfect */
 deleteNotes = (id) => {
     let data = JSON.parse(localStorage.getItem('notes'));
     data.splice(id, 1);
@@ -203,52 +170,29 @@ deleteNotes = (id) => {
     location = window.location;
 }
 
+/* Perfect */
 editNote = (id) => {
-    let title = document.getElementById('noteTitle').value.toUpperCase();
+    let title = document.getElementById('noteTitle').value;
     let description = document.getElementById('noteDescp').value;
-    if (description.length == 0) { description = '-' };
-    let priority = document.getElementById('priority').innerText;
-    const status = "pending";
+    let date = document.getElementById('noteDate').value;
+    if (date.length == 0) { let d = new Date(); date = d.getFullYear() + "-" + d.getMonth() + 1 + "-" + d.getDate(); }
+    let status = "pending";
 
     if (title.length == 0) {
         document.getElementById('emptyField').style.display = 'block';
     }
     else {
-        let data;
-        let success = document.getElementById('success')
-        if (localStorage.getItem('notes') == null) {
-            success.classList.remove('alert-sucess');
-            success.classList.add('alert-danger');
-            success.style.display = 'block';
-            success.innerText = "An unknown error occurred..!! Refreshing page..."
-            location = window.location;
-        }
-        else {
-            let already = false;
-            data = JSON.parse(localStorage.getItem('notes'));
-            data.forEach(item => {
-                if (item["title"] == title) {
-                    if (data.indexOf(item) != id) {
-                        already = true;
-                    }
-                }
-            })
-            if (already != true) {
-                data[id] = {"title": title, "description": description, "priority": priority, "status": status};
-                data = JSON.stringify(data);
-                localStorage.setItem('notes', data);
-                location = window.location;
-                success.innerText = `Note updated successfully: ${title}..!!`;
-                success.style.display = 'block';
-            }
-            else {
-                document.getElementById('alreadyField').style.display = 'block';
-            }
+        let data = JSON.parse(localStorage.getItem('notes'));
 
-        }
+        data[id] = {"title": title, "description": description, "date": date, "status": status};
+        data = JSON.stringify(data);
+        localStorage.setItem('notes', data);
+
+        window.location = location;
     }
 }
 
+/* Perfect */
 editButton = (id) => {
     document.querySelector('h1').innerText = "Editor..!!"
     document.getElementById('btn-container').innerHTML = `
@@ -258,18 +202,17 @@ editButton = (id) => {
     let data = JSON.parse(localStorage.getItem('notes'));
     document.getElementById('noteTitle').value = data[id]["title"];
     document.getElementById('noteDescp').value = data[id]["description"];
+    document.getElementById('noteDate').value = data[id]["date"];
 
-    let priority = data[id]["priority"].toLowerCase();
-    if (priority == "maximum") { check = 0 } else if (priority == "normal") { check = 1 } else if (priority == "minimum") { check = 2 };
-    document.getElementsByClassName("priority")[check].checked = true;
-    priorityTextUpdate();
     document.documentElement.scrollTop = 0;
 }
 
+/* Perfect */
 editCancel = () => {
     location = window.location;
 }
 
+/* Perfect */
 toPending = (id) => {
     let data = JSON.parse(localStorage.getItem('notes'));
     data[id]["status"] = "pending";
@@ -278,6 +221,7 @@ toPending = (id) => {
     showNotes();
 }
 
+/* Perfect */
 toDone = (id) => {
     let data = JSON.parse(localStorage.getItem('notes'));
     data[id]["status"] = "done";
@@ -286,9 +230,10 @@ toDone = (id) => {
     showNotes();
 }
 
+/* Perfect */
 darkLight = () => {
     let theme = document.getElementById('dark-light').checked;
-    if (theme == true){
+    if (theme == true) {
         var bColor = "#222";
         var hLabel = "#fff";
         var input = "#222";
@@ -296,7 +241,7 @@ darkLight = () => {
         var dropdown = "#111";
         var stheme = "dark"
     }
-    else{
+    else {
         var bColor = "#fff"
         var hLabel = "#000";
         var input = "#e6e6e6";
@@ -305,68 +250,67 @@ darkLight = () => {
         var stheme = "light";
     }
     document.querySelector("body").style.background = bColor;
-    document.querySelectorAll("h1").forEach(element =>{
+
+    document.querySelectorAll("h1").forEach(element => {
         element.style.color = hLabel;
     });
-    document.querySelectorAll("label").forEach(element =>{
+
+    document.querySelectorAll("label").forEach(element => {
         element.style.color = hLabel;
     });
+
     document.getElementById("search").style.background = input;
     document.getElementById("noteTitle").style.background = input;
+    document.getElementById("noteDate").style.background = input;
+
     document.getElementById("search").style.color = hLabel;
     document.getElementById("noteTitle").style.color = hLabel;
+    document.getElementById("noteDate").style.color = hLabel;
 
     document.querySelector("hr").style.background = hLabel;
 
     document.querySelector("textarea").style.background = input;
     document.querySelector("textarea").style.color = hLabel;
+
     document.querySelector(".card").style.background = card;
-    document.querySelectorAll(".dropdown-menu").forEach(element =>{
+    
+    document.querySelectorAll(".dropdown-menu").forEach(element => {
         element.style.background = dropdown;
     });
 
     localStorage.setItem('theme', stheme);
 }
 
+/* Perfect */
 searchNotes = () => {
     let query = document.getElementById('search').value.toLowerCase();
     let title = document.querySelectorAll('.card-title');
-    let description = document.querySelectorAll('.card-text');
-    
+
     let match = [];
     let nonmatch = [];
 
     title.forEach(element => {
-        if (element.innerText.toLowerCase().indexOf(query) > -1) {match.push(element)} else {nonmatch.push(element)};
-    });
-    description.forEach(element => {
-        if (element.innerText.toLowerCase().indexOf(query) > -1) {match.push(element)} else {nonmatch.push(element)};
+        if (element.innerText.toLowerCase().indexOf(query) > -1) { match.push(element) } else { nonmatch.push(element) };
     });
 
-    nonmatch.forEach(element =>{
-        element.parentElement.parentElement.style.display = "none" 
+    nonmatch.forEach(element => {
+        element.parentElement.parentElement.style.display = "none"
     });
 
-    match.forEach(element =>{
-        element.parentElement.parentElement.style.display = "block" 
+    match.forEach(element => {
+        element.parentElement.parentElement.style.display = "block"
     });
 }
 
+/* Perfect */
 initialRun = () => {
     let theme = localStorage.getItem('theme');
-    if (theme==null){theme = "light";localStorage.setItem('theme','light')};
-    if (theme=="dark"){document.getElementById("dark-light").checked = true};
+    if (theme == null) { theme = "light"; localStorage.setItem('theme', 'light') };
+    if (theme == "dark") { document.getElementById("dark-light").checked = true };
     darkLight();
 
-    document.getElementsByClassName("priority")[1].checked = true;
-    priorityTextUpdate();
-
-    document.querySelectorAll(".priority").forEach(item => {
-        item.addEventListener('input', priorityTextUpdate);
-    })
-
     document.getElementById("dark-light").addEventListener("input", darkLight);
-    
+
     showNotes();
 
     document.getElementById('search').addEventListener('input', searchNotes);
@@ -376,6 +320,13 @@ initialRun = () => {
         document.getElementById('alreadyField').style.display = 'none';
         document.getElementById('success').style.display = 'none';
     })
+
+    date = document.getElementById('noteDate')
+    minimum = todayDate()
+
+    date.value = minimum
+    date.setAttribute("min", minimum);
+
 }
 
 initialRun();
